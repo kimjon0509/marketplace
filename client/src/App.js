137b90485b3d4be3f9bb9a89cdbab5cbfaeeff9e
John Kim <kimjon0509@gmail.com>
2020-12-20 21:25:01 -0500

    added auth and logo

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,15 @@ import './App.scss';
 import NavBar from './navbar/navbar';
 import ImageSlider from './image-slider/imageSlider';
 import TodaysDeal from './todays-deal/todaysDeal';
+import Authentication from './authentication/authentication';
 
 function App() {
   return (
     <>
+      <Authentication></Authentication>
+      <div>
+        <img src="logo" alt="logo" name="logo"></img>
+      </div>
       <NavBar></NavBar>
       <ImageSlider></ImageSlider> 
       <TodaysDeal></TodaysDeal>
---
+++ client/src/authentication/authentication.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './authentication.scss';
+
+export default function Authentication(props){
+  return (
+    <div className='auth'>
+      <a href='/'>Sign Up</a>
+      <a href='/'>Login</a>
+    </div>
+  )
+}
